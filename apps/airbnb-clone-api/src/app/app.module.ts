28efa7d7f6789c55ddddbdf145751278b0e597b1
EikerYejan <eikeryejan@gmail.com>
2021-12-11 19:52:02 -0400

    feat(api): implement rate limiting

--- apps/airbnb-clone-api/src/app/app.module.ts
+++ apps/airbnb-clone-api/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common'
-import { ConfigModule } from '@nestjs/config'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler'
 import { APP_GUARD } from '@nestjs/core'
 import * as path from 'path'
 import { ListingsModule } from '../listings/listings.module'
@@ -13,8 +14,20 @@ const envFilePath = path.join(process.cwd(), 'apps/airbnb-clone-api/.env')
   imports: [
     ListingsModule,
     ConfigModule.forRoot({ isGlobal: true, envFilePath, load: [loadConfig] }),
+    ThrottlerModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        ttl: config.get('throttleTtl'),
+        limit: config.get('throttleLimit'),
+      }),
+    }),
   ],
   controllers: [AppController],
-  providers: [AppService, { provide: APP_GUARD, useClass: AuthGuard }],
+  providers: [
+    AppService,
+    { provide: APP_GUARD, useClass: AuthGuard },
+    { provide: APP_GUARD, useClass: ThrottlerGuard },
+  ],
 })
 export class AppModule {}
--- apps/airbnb-clone-api/src/config/index.ts
+++ apps/airbnb-clone-api/src/config/index.ts
@@ -2,6 +2,8 @@ export const loadConfig = () => {
   return {
     apiKey: process.env.API_KEY,
     databaseUrl: process.env.DATABASE_URL,
+    throttleTtl: process.env.THROTTLE_TTL,
+    throttleLimit: process.env.THROTTLE_LIMIT,
   }
 }
 
