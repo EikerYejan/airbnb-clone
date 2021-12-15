447e0349904af91db728ca91a4ff6bbd42b2f5a3
EikerYejan <eikeryejan@gmail.com>
2021-12-14 20:48:05 -0400

    feat(api): add delete and create listing mutations

--- apps/airbnb-clone-api/src/graphql/graphql.service.ts
+++ apps/airbnb-clone-api/src/graphql/graphql.service.ts
@@ -1,7 +1,7 @@
 import { Resolver, Query, Args, Mutation } from '@nestjs/graphql'
 import { ListingsUtilsService } from '../listings/listings-utils.service'
 import { ListingsService } from '../listings/listings.service'
-import { GetListings, UpdateListing } from './graphql.typings'
+import { CreateListing, GetListings, UpdateListing } from './graphql.typings'
 
 @Resolver('Listing')
 export class GraphqlService {
@@ -18,12 +18,22 @@ export class GraphqlService {
   }
 
   @Query('listing')
-  listSingleQuery(@Args('id') id: string) {
+  listSingle(@Args('id') id: string) {
     return this.listings.get({ where: { id } })
   }
 
   @Mutation('updateListing')
   updateListing(@Args('id') id: string, @Args('data') data?: UpdateListing) {
     return this.listings.update({ data, where: { id } })
+  }
+
+  @Mutation('deleteListing')
+  deleteListing(@Args('id') id: string) {
+    return this.listings.delete({ where: { id } })
+  }
+
+  @Mutation('createListing')
+  createListing(@Args('data') data: CreateListing) {
+    return this.listings.create({ data })
   }
 }
