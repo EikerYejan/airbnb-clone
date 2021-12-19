02d4818ac9a2a31c392c0eeb592ab04b86d6eba0
EikerYejan <eikeryejan@gmail.com>
2021-12-19 13:47:05 -0400

    test: update testing module

--- apps/airbnb-clone-api/__tests__/testingModule.ts
+++ apps/airbnb-clone-api/__tests__/testingModule.ts
@@ -1,4 +1,5 @@
 import { HttpModule } from '@nestjs/axios'
+import { ConfigModule } from '@nestjs/config'
 import { TerminusModule } from '@nestjs/terminus'
 import { Test } from '@nestjs/testing'
 import { AppController } from '../src/app/app.controller'
@@ -7,7 +8,7 @@ import { ListingsModule } from '../src/listings/listings.module'
 
 export const createTestingModule = () => {
   return Test.createTestingModule({
-    imports: [ListingsModule, HttpModule, TerminusModule],
+    imports: [ListingsModule, HttpModule, TerminusModule, ConfigModule.forRoot({ isGlobal: true })],
     controllers: [AppController],
     providers: [AppService],
   })
